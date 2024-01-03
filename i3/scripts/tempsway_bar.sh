c97909e52b9ea5b1ea71f8a53c0a0f44673420b5
Blake Wilson <wilso692@purdue.edu>
2024-01-03 13:49:43 -0800

    Added i3

---
+++ i3/scripts/tempsway_bar.sh
@@ -0,0 +1,13 @@
+# Change this according to your device
+################
+# Variables
+################
+
+# Keyboard input name
+#keyboard_input_name="1:1:AT_Translated_Set_2_keyboard"
+
+# Date and time
+#date_and_week=$(date "+%Y/%m/%d (w%-V)")
+#current_time=$(date "+%H:%M")
+
+echo "Hi"
